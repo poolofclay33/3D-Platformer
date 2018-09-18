50c86d9f90f158821002600dc6db16c9787bcfad
Brady <poolofclay33@gmail.com>
2018-09-18 15:14:54 -0500

    Edit (16)
    - Added timer

---
+++ 3D Platformer/Assets/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    static float timer = 0.0f;
+    public TMP_Text _txt;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        _txt.text = timer.ToString("0:00.00");
+    }
+}
