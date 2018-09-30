4bc39d55a8c8df650c327b731d2393801040c8b5
Brady Claypool <poolofclay33@gmail.com>
2018-09-30 01:48:25 -0500

    Edit (18)
    - Worked on Main Menu

---
+++ 3D Platformer/Assets/Scripts/ClickTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickTest : MonoBehaviour {
+
+    public Material _cubeHighlight;
+    public Material _cubeColor;
+    public Material _cubeClicked;
+    public Animator _playerAnim;
+
+	private void OnMouseDown()
+	{
+        StartCoroutine("startGameAnim");
+        GetComponent<Renderer>().material = _cubeClicked;
+        //sound on click
+	}
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material = _cubeHighlight;
+        //sound when entered
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = _cubeColor;
+        //possible sound when exited
+    }
+
+    IEnumerator startGameAnim()
+    {
+        _playerAnim.Play("Turn_and_Run");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Testing");
+    }
+}
---
+++ 3D Platformer/Assets/SoundSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSettings : MonoBehaviour {
+
+    public GameObject _cube;
+    public Material _cubeHighlight;
+    public Material _cubeColor;
+    public Material _cubeClicked;
+
+    private void OnMouseDown()
+    {
+        GetComponent<Renderer>().material = _cubeClicked;
+        StartCoroutine("Clicked");
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material = _cubeHighlight;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = _cubeColor;
+    }
+
+    public float _downSpeed;
+
+    IEnumerator Clicked()
+    {
+        _cube.gameObject.SetActive(true);
+        _cube.GetComponent<Rigidbody>().AddForce(0, _downSpeed, 0, ForceMode.Impulse);
+        yield return new WaitForSeconds(.1f);
+    }
+}
